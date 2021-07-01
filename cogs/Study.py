af090d5803cb2a3fe37ad1b829abec85c77d60b5
Kprjjl <69335092+Kprjjl@users.noreply.github.com>
2021-07-01 16:36:10 +0800

    Added mini reminder in Study.py

--- cogs/Study.py
+++ cogs/Study.py
@@ -2,6 +2,7 @@ import discord
 import re
 import asyncio
 from discord.ext import commands, tasks
+import datetime as dt
 
 # ex: "25-5-15 (0/4) w || 25m 0s" OR "50-10 b || 50m 0s"
 # format: "<work time>-<short break>-<interval break> (<interval progress>/<max interval>) <state> || <mins>m 0s
@@ -9,6 +10,8 @@ from discord.ext import commands, tasks
 # <state> values: "w" for work, "b" for short break, "B" for interval break
 pomo_channel_regex = ["^(\d+)-(\d+)-(\d+) \((\d+)\/(\d+)\) (w|b|B) \|{2} (\d+)m 0s$",
                       "^(\d+)-(\d+) (w|b) \|{2} (\d+)m 0s$"]
+
+
 # regex matches would be size 7 or 4
 
 
@@ -21,6 +24,7 @@ class Study(commands.Cog):
         self.pomo_channels = []
 
         self.remind_list = {}
+        self.objective_ch = None
 
     # -------- pomodoro stuff ----------
     @staticmethod
@@ -33,7 +37,7 @@ class Study(commands.Cog):
             configs = configs[0]
             configs = [int(configs[i]) if i != 2 else configs[i] for i in range(4)]
             if configs[-2] == 'B':
-                return False    # size 4 configs do not include long breaks
+                return False  # size 4 configs do not include long breaks
             size = 4
         else:
             return False
@@ -56,18 +60,18 @@ class Study(commands.Cog):
 
         # <mins> should be less than or equal to corresponding time setting depending on <state>
         if configs[-2] == 'w':
-            if configs[-1] <= configs[0]:   # <work time>
+            if configs[-1] <= configs[0]:  # <work time>
                 return configs, size
         elif configs[-2] == 'b':
-            if configs[-1] <= configs[1]:   # <short break>
+            if configs[-1] <= configs[1]:  # <short break>
                 return configs, size
         elif configs[-2] == 'B':
-            if configs[-1] <= configs[2]:   # <long break>
+            if configs[-1] <= configs[2]:  # <long break>
                 return configs, size
         else:
             return False
 
-        return False    # channel does not fit the criteria
+        return False  # channel does not fit the criteria
 
     @tasks.loop(minutes=5)
     async def pomo_update(self):
@@ -96,9 +100,9 @@ class Study(commands.Cog):
                                 cfg[3] = 0  # reset
                                 cfg[-1], cfg[-2] = cfg[2], 'B'  # start long break
                         elif size == 4:
-                            cfg[-1], cfg[-2] = (cfg[1], 'b')
+                            cfg[-1], cfg[-2] = cfg[1], 'b'
                     elif cfg[-2] == 'b' or cfg[-2] == 'B':
-                        cfg[-1], cfg[-2] = (cfg[0], 'w')
+                        cfg[-1], cfg[-2] = cfg[0], 'w'
 
                 # New name construction (size == 7 ex: "25-5-15 (2/4) 'b' || 5m 0s")
                 new_name = f"{cfg[0]}-{cfg[1]}"  # "25-5"
@@ -142,12 +146,65 @@ class Study(commands.Cog):
             await ctx.send("Pomodoro timers have been cancelled.")
         else:
             await ctx.send("Timers are already stopped.")
+
     # ---------------------------
 
     # ---------- mini reminder ----------
+    @commands.command(name="init_ob_ch")
+    async def init_objective_ch(self, ctx):
+        self.objective_ch = ctx.channel
+        await ctx.send(f"Objectives channel set to `{ctx.channel.name}`")
+
     @commands.command()
-    async def objective(self, ctx, *, obj):
-        pass
+    async def objective(self, ctx, *, arg):
+        if self.objective_ch is None:
+            return await ctx.send(f"Please use `init_ob_ch` command to set objective channel")
+        try:
+            obj, *time = re.findall("([\w\W]+)>>\s*(\d+(hrs?|m|h))[,\s]*(\d+(hrs?|m|h))?", arg)[0]
+        except ValueError or IndexError:
+            await ctx.send("Follow `objective` >> `#hr`, `#m` format.")
+        else:
+            time = [t for t in time if t]
+            obj = obj.strip()
+            time_d = {}
+
+            def time_gen(t):
+                for _ in t:
+                    yield _
+            time = time_gen(time)
+            prev = next(time)
+            while True:
+                try:
+                    current = next(time)
+                except StopIteration:
+                    break
+                if current in ['hrs', 'hr', 'h', 'm']:
+                    if current == 'hrs' or current == 'hr':
+                        current = 'h'
+                    time_d[current] = int(prev[:-1])
+                else:
+                    prev = current
+
+            # allocated_time = dt.timedelta(hours=time_d.get('h', 0), minutes=time_d.get('m', 0))
+
+            # self.remind_list[ctx.author] = (obj, allocated_time, dt.datetime.now())
+            self.remind_list[ctx.author] = (obj, time_d, dt.datetime.now())
+            if not self.remind_objective.is_running():
+                self.remind_objective.start()
+            await ctx.send(f"{ctx.author.mention} has listed an objective of `{obj}`.")
+
+    @tasks.loop(minutes=1)
+    async def remind_objective(self):
+        for member in self.remind_list:
+            hours = self.remind_list[member][1].get('h', 0)
+            minutes = self.remind_list[member][1].get('m', 0)
+            if dt.datetime.now() - self.remind_list[member][2] >= dt.timedelta(hours=hours, minutes=minutes):
+                await self.objective_ch.send(f"{member.mention}, it has been {hours}h, {minutes}m since "
+                                             f"you listed your objective: `{self.remind_list[member][0]}`")
+            del self.remind_list[member]
+
+        if not self.remind_list:
+            return self.remind_objective.cancel()
 
 
 def setup(bot):
